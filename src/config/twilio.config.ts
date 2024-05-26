92c1ca7ad68e49101f5b2bbdb8e1227a2fc4cb9f
Darién Julián Ramírez <ianshalaga@gmail.com>
2024-05-26 23:56:02 +0200

    twilio.config updated

--- src/config/twilio.config.ts
+++ src/config/twilio.config.ts
@@ -1,6 +1,7 @@
 import twilio from "twilio";
 import config from "./env.config";
 
-const clientTwilio = twilio(config.twilioAccountSid, config.twilioAuthToken);
+// const clientTwilio = twilio(config.twilioAccountSid, config.twilioAuthToken);
+const clientTwilio = "";
 
 export default clientTwilio;
